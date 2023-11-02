19895ac0bf2831bdbd568082b4e1bd529dd57531
MINHA-J <harororo721@khu.ac.kr>
2023-11-02 15:23:22 +0900

    Set XRI

---
+++ Assets/Scripts/HandData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandData : MonoBehaviour
+{
+    public enum HandModelType {Left, Right}
+
+    public HandModelType handModelType;
+    public Transform root;
+    public Animator animator;
+    public Transform[] fingerBones;
+
+}

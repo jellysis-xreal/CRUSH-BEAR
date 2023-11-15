fd01b6354d4d3afb4ba6ed1bc98da5316c92fd38
cmins1011 <chlalstj1005@gmail.com>
2023-11-15 01:19:48 -0500

    add Free fall

--- Assets/Scripts/Avoid/Laser.cs
+++ Assets/Scripts/Avoid/Laser.cs
@@ -5,22 +5,29 @@ using UnityEngine;
 public class Laser : MonoBehaviour
 {
     public LineRenderer lr;
+    public Transform playerTransform;
+    private Vector3 dir;
     // Start is called before the first frame update
+    
     void Start()
     {
         lr = GetComponent<LineRenderer>();
+        playerTransform = GameObject.FindWithTag("MainCamera").transform;
+        dir = playerTransform.position - transform.position;
+        dir.Normalize();
     }
 
     // Update is called once per frame
     void Update()
     {
-        lr.SetPosition(0, transform.position);
+        // lr.SetPosition(0, transform.position);
         RaycastHit hit;
-        if(Physics.Raycast(transform.position, transform.forward, out hit)){
+        if(Physics.Raycast(transform.position, dir, out hit)){
             if (hit.collider)
             {
                 lr.SetPosition(1, hit.point);
-            } else lr.SetPosition(1, transform.forward*5000);
+                // Debug.Log("[TEST] position: " + transform.position + " forward: " + transform.forward);
+            } else lr.SetPosition(1, dir*5000);
         }
     }
 }

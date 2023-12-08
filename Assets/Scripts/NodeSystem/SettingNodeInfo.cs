43b1eb2d00824ec7da2ed60e4ea94eaf93d09c83
kkkkiiiiii <98201682+kkkkiiiiii@users.noreply.github.com>
2023-12-09 00:09:43 +0900

    edit node arrival time, add property
    - NodeInfo.cs
    - SettingNodeInfo

--- Assets/Scripts/NodeSystem/SettingNodeInfo.cs
+++ Assets/Scripts/NodeSystem/SettingNodeInfo.cs
@@ -8,11 +8,18 @@ public class SettingNodeInfo : MonoBehaviour
     public NodeInfo nodeInfo;
     [Header("Set Node Info! (It's up to the developer)")] 
     [SerializeField] private float generationTime;
-    [SerializeField] private int arrivalAreaIndex;
-    [SerializeField] private float movingSpeed;
+    [SerializeField] private float timeToReachPlayer; // 소환 이후 Arrival Area까지의 도달 시간
+    [SerializeField] private int arrivalAreaIndex; // 목표로 하는 area의 인덱스
     [SerializeField] private ObjectType objectType;
     [SerializeField] private MovingType movingType;
     [SerializeField] private ObjectNum objectNum;
+    
+    [Header("MovingType : Straight")]
+    [SerializeField] private float movingSpeed;
+    
+    [Header("MovingType : JumpMoving")]
+    public int totalJumpNumberOfTimes; // area에 도달하기 까지 chd 점프할 횟수 
+
     public enum ObjectType
     {
         Rip,
@@ -43,11 +50,13 @@ public class SettingNodeInfo : MonoBehaviour
         nodeInfo.posY = (float)Math.Round(transform.position.y,2);
         nodeInfo.posZ = (float)Math.Round(transform.position.z,2);
         nodeInfo.generationTime = generationTime;
+        nodeInfo.timeToReachPlayer = timeToReachPlayer;
         nodeInfo.arrivalAreaIndex = arrivalAreaIndex;
         nodeInfo.movingSpeed = movingSpeed;
         nodeInfo.objectType = objectType.ToString();
         nodeInfo.movingType = movingType.ToString();
         string objectIndex = objectNum.ToString().Substring(objectNum.ToString().Length - 1);
         nodeInfo.objectNum = int.Parse(objectIndex);
+        nodeInfo.totalJumpNumberOfTimes = totalJumpNumberOfTimes;
     }
 }

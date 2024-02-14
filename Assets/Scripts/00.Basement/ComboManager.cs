1d95fe22b6661ebedb9a742a57d7540cf51a78f2
kkkkiiiiii <98201682+kkkkiiiiii@users.noreply.github.com>
2024-02-15 00:46:00 +0900

    add : combo system

--- Assets/Scripts/00.Basement/ComboManager.cs
+++ Assets/Scripts/00.Basement/ComboManager.cs
@@ -9,12 +9,14 @@ using UnityEngine.UI;
 public class ComboManager : MonoBehaviour
 {
     // 플레이어의 인터랙션 결과에 따라 콤보 시스템을 업데이트하는 스크립트 
-    public float comboValue = 0f;
+    public int comboValue = 0;
+    public float comboValueFever = 0f;
     public float comboMultiflier = 0f;
     public Coroutine comboCoroutine = null;
     public Slider comboSlider;
     public TextMeshPro comboMultiflierTMP;
     public TMP_Text comboMultiflierTMPd;
+    public TMP_Text comboValueTMP;
     
     public void GameStart()
     {
@@ -40,35 +42,39 @@ public class ComboManager : MonoBehaviour
     }
     public void ActionSucceed()
     {
-        comboValue += 0.05f;
-        if(comboValue > 1f) comboValue = 1f;
+        comboValueFever += 0.05f;
+        comboValue += 1;
+        
+        if(comboValueFever > 1f) comboValueFever = 1f;
     }
     public void ActionFailed()
     {
-        comboValue -= 0.05f;
-        if(comboValue < 0f) comboValue = 0f;
+        comboValueFever -= 0.05f;
+        comboValue = 0;
+        if(comboValueFever < 0f) comboValueFever = 0f;
     }
     public void ActionMissed()
     {
-        comboValue -= 0.05f;
-        if(comboValue < 0f) comboValue = 0f;
+        comboValueFever -= 0.05f;
+        if(comboValueFever < 0f) comboValueFever = 0f;
     }
 
     public void SetComboMultiflier(float value)
     {
-        comboMultiflier = 1f;
+        /* comboMultiflier = 1f;
         if (value > 0.8f) comboMultiflier = 8f;
         else if (value > 0.6f) comboMultiflier = 4f;
         else if (value > 0.4f) comboMultiflier = 2f;
         else comboMultiflier = 1f;
 
-        comboMultiflierTMPd.text = $"x {comboMultiflier}";
+        comboMultiflierTMPd.text = $"x {comboMultiflier}"; */
+        comboValueTMP.text = $"Combo {comboValue}";
     }
 
     public void InitComboUI()
     {
-        comboValue = 0f;
-        comboSlider.value = comboValue;
+        // comboValueFever = 0f;
+        // comboSlider.value = comboValueFever;
         comboMultiflier = 1f;
         comboMultiflierTMPd.text = $"x {comboMultiflier}";
     }
@@ -77,11 +83,11 @@ public class ComboManager : MonoBehaviour
         float waitSecond = 0.1f;
         while (true)
         {
-            comboValue -= Time.fixedDeltaTime * 0.5f;
-            if(comboValue < 0f) comboValue = 0f;
-            comboSlider.value = comboValue;
-            SetComboMultiflier(comboValue);
-            Debug.Log($"update combo {comboValue} deltaTime {Time.deltaTime}");
+            // comboValueFever -= Time.fixedDeltaTime * 0.5f;
+            // if(comboValueFever < 0f) comboValueFever = 0f;
+            // comboSlider.value = comboValueFever;
+            SetComboMultiflier(comboValueFever);
+            // Debug.Log($"update combo {comboValueFever} deltaTime {Time.deltaTime}");
 
             yield return new WaitForSeconds(waitSecond);
         }

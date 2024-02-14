0a9b02178f58ce07aed1e3a48eab11d0cdb22142
evyfjae <46910279+evyfjae@users.noreply.github.com>
2024-02-14 16:23:34 +0900

    diary setting_press0
    0 번을 누르면 다이어리가 1장씩 넘어갑니다. 텍스트는 TMP 세팅만 하여 넣어두었고, 애니메이션은 안들어가 있습니다.

--- Assets/Art/Models/10.Lobby_Diary/Animation/DiaryAniController.cs
+++ Assets/Art/Models/10.Lobby_Diary/Animation/DiaryAniController.cs
@@ -6,6 +6,8 @@ using UnityEngine.InputSystem.LowLevel;
 public class DiaryAniController : MonoBehaviour
 {
     public Animator scene1_Anim, scene2_Anim, scene3_Anim, scene4_Anim;
+    public Animator book_Anim;
+    public int bookPage = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -46,6 +48,66 @@ public class DiaryAniController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha8))
         {
             scene4_Anim.SetTrigger("close");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            nextPage();
+        }
+    }
+
+    public void nextPage()
+    {
+        switch(bookPage)
+        {
+            case 0:
+                bookPage++;
+                book_Anim.SetTrigger("next");
+                StartCoroutine(openPage());
+                break;
+            case 1:
+                bookPage++;
+                scene1_Anim.SetTrigger("close");
+                book_Anim.SetTrigger("next");
+                StartCoroutine(openPage());
+                break;
+            case 2:
+                bookPage++;
+                scene2_Anim.SetTrigger("close");
+                book_Anim.SetTrigger("next");
+                StartCoroutine(openPage());
+                break;
+            case 3:
+                bookPage++;
+                scene3_Anim.SetTrigger("close");
+                book_Anim.SetTrigger("next");
+                StartCoroutine(openPage());
+                break;
+            case 4:
+                bookPage = 0;
+                scene4_Anim.SetTrigger("close");
+                //book_Anim.SetTrigger("next");
+                //StartCoroutine(openPage());
+                break;
+        }
+    }
+
+    IEnumerator openPage()
+    {
+        yield return new WaitForSeconds(1.0f);
+        switch (bookPage)
+        {
+            case 1:
+                scene1_Anim.SetTrigger("open");
+                break;
+            case 2:
+                scene2_Anim.SetTrigger("open");
+                break;
+            case 3:
+                scene3_Anim.SetTrigger("open");
+                break;
+            case 4:
+                scene4_Anim.SetTrigger("open");
+                break;
         }
     }
 }

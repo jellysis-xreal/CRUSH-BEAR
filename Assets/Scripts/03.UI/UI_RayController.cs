b2861fb3885d5741089c2a8bc280510e01ff9c43
cmins1011 <chlalstj1005@gmail.com>
2024-02-16 01:15:35 +0900

    [UI] ray

--- Assets/Scripts/03.UI/UI_RayController.cs
+++ Assets/Scripts/03.UI/UI_RayController.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.SceneManagement;
 
 public class UI_RayController : MonoBehaviour
 {
     [SerializeField] private InputActionAsset m_ActionAsset;
     public InputActionProperty menuAction;
-    public XRInteractorLineVisual ray_left;
-    public XRInteractorLineVisual ray_right;
+    public GameObject ray_left;
+    public GameObject ray_right;
     
     // Start is called before the first frame update
     void Start()
@@ -20,23 +21,28 @@ public class UI_RayController : MonoBehaviour
 
     void Update()
     {
-        ray_left.enabled = GameManager.UI.IsRayOn();
-        ray_right.enabled = GameManager.UI.IsRayOn();
+        ray_left.SetActive(GameManager.UI.IsRayOn());
+        ray_right.SetActive(GameManager.UI.IsRayOn());
     }
 
     private void OnMyActionTriggered(InputAction.CallbackContext context)
     {
         // InputAction이 trigger될 때 호출되는 콜백 메서드
         Debug.Log("Action Triggered!");
-
-        if (!GameManager.UI.IsRayOn()) {
-            GameManager.Wave.SetIsPause(true);
-            GameManager.UI.SetRayOn(true);
-            GameObject go = GameManager.UI.ShowPopupUI<UI_Popup>("PopupSettings").gameObject;
-            GameManager.UI.SetCanvas(go, true);
-        }
         
-        GameManager.UI.CalibrateCanvasLocation();
+        if (SceneManager.GetActiveScene().name == "00.StartScene"){
+            GameManager.UI.SetRayOn(!GameManager.UI.IsRayOn());
+        }
+        else {
+            if (!GameManager.UI.IsRayOn())
+            {
+                GameManager.UI.SetRayOn(true);
+                GameManager.Wave.SetIsPause(true);
+                GameObject go = GameManager.UI.ShowPopupUI<UI_Popup>("PopupSettings").gameObject;
+                GameManager.UI.SetCanvas(go, true);
+            }
+            GameManager.UI.CalibrateCanvasLocation();
+        }
         // ray.enabled = ray.enabled ? false : true;
     }
 }

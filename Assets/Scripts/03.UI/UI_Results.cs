8725f22ab7304cc6b7550dff7b26418e307b325b
MINHA-J <harororo721@khu.ac.kr>
2024-06-22 22:16:45 +0900

    Update UI_Results.cs

--- Assets/Scripts/03.UI/UI_Results.cs
+++ Assets/Scripts/03.UI/UI_Results.cs
@@ -29,13 +29,15 @@ public class UI_Results : MonoBehaviour
     public void ShowResults()
     {
         // 플레이어의 위치와 방향을 가져옵니다.
-        GameObject player = GameObject.FindWithTag("Player");
+        GameObject player = GameObject.FindWithTag("MainCamera");
         Vector3 playerPosition = player.transform.position;
         Vector3 playerDirection = player.transform.forward;
 
         // 플레이어의 정면 방향으로 일정 거리만큼 이동한 위치를 계산합니다.
         float distance = 6.0f; // 이 값은 원하는 대로 조정할 수 있습니다.
-        Vector3 resultPosition = playerPosition + playerDirection * distance;
+        Vector3 resultPosition = playerPosition + 
+                                 (Vector3.up * 1.5f) + 
+                                 (playerDirection * distance);
 
         // result를 그 위치에 설정합니다.
         transform.position = resultPosition;

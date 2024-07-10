5efd4b7a08d2ac40fd625134a738843cd0a30692
wave-sound <lunar105630@gmail.com>
2024-07-10 18:18:58 +0900

    [Fix] Fixed for Popup

---
+++ Assets/Scripts/03.UI/SpriteData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SpriteData", menuName = "ScriptableObjects/SpriteData", order = 1)]
+public class SpriteData : ScriptableObject
+{
+    [SerializeField] public Sprite[] numberSprites;
+    [SerializeField] public Sprite comma;
+    [SerializeField] public Sprite dot;
+    [SerializeField] public Sprite x;
+}

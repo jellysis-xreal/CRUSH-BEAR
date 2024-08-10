a59188590c155c8c247addf0d3e6ef72526f5a44
kkkkiiiiii <alohomoratolit@naver.com>
2024-08-10 18:38:36 +0900

    [Edit] : Child Trigger 코루틴

--- Assets/Scripts/01.Interaction/Punch/ChildTriggerChecker.cs
+++ Assets/Scripts/01.Interaction/Punch/ChildTriggerChecker.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 using Motion = EnumTypes.Motion;
 
@@ -8,20 +9,13 @@ public class ChildTriggerChecker : MonoBehaviour
 {
     public bool isTriggered = false;
     public Motion handMotion = Motion.None;
-    private IEnumerator ChangeIsTriggeredField()
-    {
-        isTriggered = true;
-        // Debug.Log($"[Child] {((float)Time.time)} {transform.root.name} is Triggered {isTriggered}");
-        yield return new WaitForSeconds(1f);
-        isTriggered = false; // 1초 뒤 false로 초기화
-        yield break;
-    }
+    
     private async void OnTriggerEnter(Collider other)
     {
         if(isTriggered || !other.CompareTag("Destroyer")) return;
 
         isTriggered = true;
-        /*await Unitask
-        if () StartCoroutine(ChangeIsTriggeredField());*/
+        await UniTask.Delay(1000);
+        isTriggered = false;
     }
 }

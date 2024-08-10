feccbe6553ce9212340ce9bd15095fa34faa0b6a
kkkkiiiiii <alohomoratolit@naver.com>
2024-08-10 18:46:04 +0900

    [Edit] : Remove non-neccessary caching

--- Assets/Scripts/01.Interaction/Punch/PunchableMovement.cs
+++ Assets/Scripts/01.Interaction/Punch/PunchableMovement.cs
@@ -29,25 +29,16 @@ public class PunchableMovement : MonoBehaviour, IPunchableMovement
     private int shootStandard;
     
     // 토핑이 맞은, 맞지 않은 후에 활용할 변수
-    //private bool _isHit = false;
     private bool _isArrivalAreaHit = false; // 박스 트리거된 이후, 바로 직전의 움직임을 유지할 때 사용하는 변수
-    private MeshRenderer _meshRenderer;
-    public SpriteRenderer spriteRenderer; 
-    private Breakable _breakable;
-    private CookieControl _cookieControl;
-    void Awake()
-    {
-        _rigidbody = GetComponent<Rigidbody>();
-        _breakable = GetComponent<Breakable>();
-        _meshRenderer = GetComponent<MeshRenderer>();
-        _cookieControl = GetComponent<CookieControl>();
-    }
+    public MeshRenderer meshRenderer;
+    private SpriteRenderer _spriteRenderer; 
+    public Breakable breakable;
+    public CookieControl _cookieControl;
 
     public void StartMovement()
     {
         
     }
-  
     public void InitializeToppingRoutine(NodeInfo node)
     {
         _isArrivalAreaHit = false;
@@ -55,8 +46,8 @@ public class PunchableMovement : MonoBehaviour, IPunchableMovement
         arriveTime = node.timeToReachPlayer;
         transform.rotation = Quaternion.identity;
         
-        _meshRenderer.enabled = true;
-        if (spriteRenderer != null) spriteRenderer.enabled = true;
+        meshRenderer.enabled = true;
+        if (_spriteRenderer != null) _spriteRenderer.enabled = true;
         
         _rigidbody.velocity=Vector3.zero;
         _rigidbody.angularVelocity=Vector3.zero;
@@ -75,12 +66,12 @@ public class PunchableMovement : MonoBehaviour, IPunchableMovement
     // 손에 맞거나 뒤 trigger pad에 닿았을 경우 setActive(false)
     public void EndInteraction()
     {
-        _meshRenderer.enabled = false;
-        if(spriteRenderer != null) spriteRenderer.enabled = false;
+        meshRenderer.enabled = false;
+        if(_spriteRenderer != null) _spriteRenderer.enabled = false;
         else if(transform.childCount == 2)
         {
-            spriteRenderer = GetComponentInChildren<SpriteRenderer>();
-            spriteRenderer.enabled = false;
+            _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+            _spriteRenderer.enabled = false;
         }
         
         _rigidbody.velocity=Vector3.zero;
@@ -93,14 +84,14 @@ public class PunchableMovement : MonoBehaviour, IPunchableMovement
     async UniTask TriggerArrivalAreaEndInteraction()
     {
         await UniTask.WaitForSeconds(1);
-        _meshRenderer.enabled = false;
-        if(spriteRenderer != null) spriteRenderer.enabled = false; 
+        meshRenderer.enabled = false;
+        if(_spriteRenderer != null) _spriteRenderer.enabled = false; 
         else if(transform.childCount == 2)
         {
-            spriteRenderer = GetComponentInChildren<SpriteRenderer>();
-            spriteRenderer.enabled = false;
+            _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+            _spriteRenderer.enabled = false;
         }
-        if(!_breakable.m_Destroyed) GameManager.Score.ScoringMiss(this.gameObject);
+        if(!breakable.m_Destroyed) GameManager.Score.ScoringMiss(this.gameObject);
         
         _rigidbody.velocity=Vector3.zero;
         _rigidbody.angularVelocity=Vector3.zero;
@@ -114,8 +105,8 @@ public class PunchableMovement : MonoBehaviour, IPunchableMovement
     {
         await UniTask.WaitForSeconds(coolTime);
         transform.gameObject.SetActive(false); // coolTime 다 됐으니 비활성화
-        _breakable.m_Destroyed = false;
-        _meshRenderer.enabled = true;
+        breakable.m_Destroyed = false;
+        meshRenderer.enabled = true;
     }
     private void OnTriggerEnter(Collider other)
     {
